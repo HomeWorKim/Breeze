a1db59a546d45354cc80b2eb22af47516498590a
명윤 <100angru@gmail.com>
2018-06-08 07:54:14 +0900

    kmy
    chat_list

--- app/src/main/java/bronzetrio/breeze/fragments/ChatFragment.java
+++ app/src/main/java/bronzetrio/breeze/fragments/ChatFragment.java
@@ -8,12 +8,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.zip.Inflater;
 
 import bronzetrio.breeze.Chat_list;
 import bronzetrio.breeze.R;
@@ -33,10 +46,14 @@ public class ChatFragment extends Fragment {
     private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
 
-    private RecyclerView recyclerView;
-    private FirebaseRecyclerAdapter<Chat_list, ViewHolder2> mFirebaseAdapter;
+
     private DatabaseReference databaseReference;
-    private LinearLayoutManager mLinearLayoutManager;
+    private ListView listview;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private LayoutInflater inflater;
+    private ArrayAdapter Adapter;
+    private ArrayList<String> a;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -57,62 +74,59 @@ public class ChatFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        a=new ArrayList<>();
         //database 객체 가져오기.
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-
-        //이 어댑터는 Firebase의 database를 지속적으로 감시하며 메시지가 추가 됐을 때, 혹은
-        //액티비티가 처음 로딩 됐을 때, 메시지들을 레이아웃에 추가.
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Chat_list, ViewHolder2>(Chat_list.class, R.layout.fragment_chat, ViewHolder2.class, databaseReference.child("sample")) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("chatroom");
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        Map<String, Object> taskMap = new HashMap<String, Object>();
+        taskMap.put("/token",1);
+        databaseReference.updateChildren(taskMap);
+        Map<String, Object> taskMap2 = new HashMap<String, Object>();
+        taskMap2.put("/token",null);
+        databaseReference.updateChildren(taskMap2);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            protected void populateViewHolder(ViewHolder2 viewHolder, Chat_list chatData, int position) {
-                //viewHolder.imgv.setImageBitmap(null);
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                Log.d("귀찮아 : ",chatData.getText()+"    "+chatData.getRoom_name());
-                viewHolder.userTv.setText(chatData.getText());
-                viewHolder.msgTv.setText(chatData.getRoom_name());
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Log.d("chat : ",dataSnapshot1.getKey());
+                    int idx = dataSnapshot1.getKey().indexOf("_");
+                    String first = dataSnapshot1.getKey().substring(0,idx);
+                    String second = dataSnapshot1.getKey().substring(idx+1,dataSnapshot1.getKey().length());
+                    Log.d("chat : ",first);
+                    if(user.getUid().equals(first)){
+                        String tmp = first +"와"+second+"의 방";
 
-                //메시지를 레이아웃에 추가하는 동작들을 여기에 코드로 작성하면 된다고 함.
-            }
-        };
-        //onItemRangeInserted()안의 코드는 메시지가 많이 와서 화면에 표시할 수 있는 양보다
-        //더 많은 양의 아이템들이 추가 됐을 때, 스크롤을 가장 아래로 내려주는 코드
-        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                int friendlyMessageCount = mFirebaseAdapter.getItemCount();
-                int lasVisiblePosition =
-                        mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
-                if((lasVisiblePosition == -1) || (positionStart >= (friendlyMessageCount - 1)) && (lasVisiblePosition == (positionStart - 1))){
-                    recyclerView.scrollToPosition(positionStart);
+                        a.add(tmp);
+                    }else{
+                        continue;
+                    }
                 }
+                Log.d("a 사이즈3 : ",Integer.toString(a.size()));
+                Adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, a);
+                listview.setAdapter(Adapter);
             }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("data_error", "loadPost:onCancelled", databaseError.toException());
+
+            }
         });
 
     }
-    public static class ViewHolder2 extends RecyclerView.ViewHolder{
-        //뷰홀더 패턴에 사용할 ViewHolder
-        TextView msgTv;
-        TextView userTv;
-        ImageView imgv;
-        public ViewHolder2(View v){
-            super(v);
-            imgv = (ImageView)v.findViewById(R.id.setting);
-            msgTv = (TextView)v.findViewById(R.id.room_name);
-            userTv = (TextView)v.findViewById(R.id.text);
-        }
-    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
-        recyclerView = (RecyclerView)view;
-        mLinearLayoutManager = new LinearLayoutManager(getContext());
 
-        recyclerView.setLayoutManager(mLinearLayoutManager);
-        recyclerView.setAdapter(mFirebaseAdapter);
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+        listview = (ListView) view.findViewById(R.id.list);
+
+
+        Log.d("a 사이즈 : ",Integer.toString(a.size()));
+
         return view;
     }
 
