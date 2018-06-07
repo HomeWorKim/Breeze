c7dbee1e0465f2a4132a42a100d39ad850195f97
명윤 <100angru@gmail.com>
2018-06-08 01:48:27 +0900

    Revert "Kmy"
    This reverts commit 63c3ef5ff8299ca8749d0a818af000af48bbe5d7.

--- app/src/main/java/bronzetrio/breeze/Open_Matching.java
+++ app/src/main/java/bronzetrio/breeze/Open_Matching.java
@@ -43,29 +43,28 @@ public class Open_Matching extends AppCompatActivity {
     ArrayList<String> birthdays = new ArrayList<>();
     ArrayList<String> sexes = new ArrayList<>();
     ArrayList<String> imges = new ArrayList<>();
-    ArrayList<String> UID = new ArrayList<>();
-    int pop_idx = 0;
 
+    int pop_idx=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open__matching);
 
-        reject = (Button) findViewById(R.id.reject);
-        accept = (Button) findViewById(R.id.accept);
-        profile_img = (ImageView) findViewById(R.id.profile_img);
-        Name = (TextView) findViewById(R.id.name);
+        reject = (Button)findViewById(R.id.reject);
+        accept = (Button)findViewById(R.id.accept);
+        profile_img = (ImageView)findViewById(R.id.profile_img);
+        Name = (TextView)findViewById(R.id.name);
         //database 객체 가져오기.
         databaseReference = FirebaseDatabase.getInstance().getReference("profile");
         mAuth = FirebaseAuth.getInstance();
 
 
-        if (flag) {
+        if(flag){
             Map<String, Object> taskMap = new HashMap<String, Object>();
-            taskMap.put("/token", 1);
+            taskMap.put("/token",1);
             databaseReference.updateChildren(taskMap);
             Map<String, Object> taskMap2 = new HashMap<String, Object>();
-            taskMap2.put("/token", null);
+            taskMap2.put("/token",null);
             databaseReference.updateChildren(taskMap2);
             flag = false;
         }
@@ -75,40 +74,41 @@ public class Open_Matching extends AppCompatActivity {
                 databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot first : dataSnapshot.getChildren()) {
-                            for (DataSnapshot second : first.getChildren()) {
-                                {
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    if(first.getKey().toString().equals(user.getUid())){
-                                        continue;
+                        for(DataSnapshot first : dataSnapshot.getChildren()){
+                            for(DataSnapshot second : first.getChildren()){
+                                for(DataSnapshot third : second.getChildren()){
+                                    for(DataSnapshot fourth : third.getChildren()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        int first_idx = user.getEmail().indexOf("@");
+
+                                        if(third.getKey().toString().equals(user.getEmail().substring(0,first_idx))){
+                                            continue;
+                                        }
+                                        Log.d("match : ",third.getKey().toString());
+                                        String name = (String) fourth.child("name").getValue();
+                                        String major = (String) fourth.child("major").getValue();
+                                        String hobby = (String) fourth.child("hobby").getValue();
+                                        String year = (String) fourth.child("year").getValue();
+                                        String month = (String) fourth.child("month").getValue();
+                                        String day = (String) fourth.child("day").getValue();
+                                        String sex = (String) fourth.child("sex").getValue();
+                                        String img = (String) fourth.child("img").getValue();
+
+                                        String birth = year+"-"+month+"-"+day;
+                                        names.add(name);
+                                        majors.add(major);
+                                        hobbys.add(hobby);
+                                        birthdays.add(birth);
+                                        sexes.add(sex);
+                                        imges.add(img);
                                     }
-
-                                    Log.d("match : ", first.getKey().toString());
-                                    String name = (String) second.child("name").getValue();
-                                    String major = (String) second.child("major").getValue();
-                                    String hobby = (String) second.child("hobby").getValue();
-                                    String year = (String) second.child("year").getValue();
-                                    String month = (String) second.child("month").getValue();
-                                    String day = (String) second.child("day").getValue();
-                                    String sex = (String) second.child("sex").getValue();
-                                    String img = (String) second.child("img").getValue();
-                                    String uid = (String) first.getKey().toString();
-
-                                    String birth = year + "-" + month + "-" + day;
-                                    names.add(name);
-                                    majors.add(major);
-                                    hobbys.add(hobby);
-                                    birthdays.add(birth);
-                                    sexes.add(sex);
-                                    imges.add(img);
-                                    UID.add(uid);
                                 }
                             }
                         }
-                        int random = (int) (Math.random() * (names.size() - 1));
+                        int random = (int)(Math.random()*(names.size()-1));
                         Name.setText(names.get(random));
                         profile_img.setImageBitmap(StringToBitMap(imges.get(random)));
-                        pop_idx = random;
+                        pop_idx=random;
                     }
 
                     @Override
@@ -118,24 +118,23 @@ public class Open_Matching extends AppCompatActivity {
                 });
             }
         };
-        View.OnClickListener listener = new View.OnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 int i = v.getId();
                 //재매칭 클릭.
-                if (i == R.id.reject) {
-                    Log.d("Size : ", Integer.toString(names.size()));
-                    if (names.size() == 1) {
-                        Toast.makeText(getApplicationContext(), "매칭을 진행 할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                if(i == R.id.reject) {
+                    Log.d("Size : ",Integer.toString(names.size()));
+                    if(names.size() == 1){
+                        Toast.makeText(getApplicationContext(),"매칭을 진행 할 수 없습니다.",Toast.LENGTH_SHORT).show();
 
-                    } else {
+                    }else {
                         names.remove(pop_idx);
                         majors.remove(pop_idx);
                         hobbys.remove(pop_idx);
                         birthdays.remove(pop_idx);
                         sexes.remove(pop_idx);
                         imges.remove(pop_idx);
-                        UID.remove(pop_idx);
 
                         int random = (int) (Math.random() * (names.size() - 1));
                         //Log.d("Size : ",Integer.toString(random));
@@ -144,14 +143,16 @@ public class Open_Matching extends AppCompatActivity {
                     }
                 }
                 //승락 클릭.
-                else if (i == R.id.accept) {
+                else if(i == R.id.accept){
                     FirebaseUser user = mAuth.getCurrentUser();
-                    String uid = user.getUid();
-                    Intent intent = new Intent(Open_Matching.this, Chat_Room_v2.class);
-                    intent.putExtra("first_uid",uid);
-                    intent.putExtra("second_uid",UID.get(pop_idx));
-                    startActivity(intent);
-                    finish();
+                    String email = user.getEmail();
+                    int first_idx = email.indexOf("@");
+                    String first = email.substring(0,first_idx);
+                    String tmp = email.substring(first_idx+1,email.length());
+                    Log.d("string",first_idx+"  "+first+"   "+tmp);
+                    int second_idx = tmp.indexOf(".");
+                    String second = tmp.substring(0,second_idx);
+                    String last = tmp.substring(second_idx+1,tmp.length());
                 }
 
             }
@@ -166,7 +167,6 @@ public class Open_Matching extends AppCompatActivity {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
-
     @Override
     public void onStop() {
         super.onStop();
@@ -175,12 +175,12 @@ public class Open_Matching extends AppCompatActivity {
         }
     }
 
-    public Bitmap StringToBitMap(String encodedString) {
-        try {
-            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte= Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
             return bitmap;
-        } catch (Exception e) {
+        }catch(Exception e){
             e.getMessage();
             return null;
         }
